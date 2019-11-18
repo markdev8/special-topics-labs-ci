c05c8876dec18672a2314e4022b54860459c31cc
Mark Beach <parapetenia@hotmail.com>
2019-11-18 18:51:15 -0500

    completed filepath for local trial. trying for a successful build.  console output on failure says unexpected last character ''' in junit 'build/reports/**/*.xml' line

--- docker/dsl/build_pipeline.groovy
+++ docker/dsl/build_pipeline.groovy
@@ -2,7 +2,7 @@ pipelineJob('build-pipeline-job') {
 
   // This is the repo that contains the Jenkinsfile you want to use to define your pipeline \switched
   //def repo = 'https://github.com/markdev8/special-topics-labs-ci'  // \changed to my repo
-  def repo = '/special-topics-labs/special-topics-labs-ci'  // \for local attempt
+  def repo = '/home/beachm4/special-topics-labs/special-topics-labs-ci'  // \for local attempt
 
   description("Your App Pipeline") 
   keepDependencies(false) 
